348a9d62ab063d755a2879097b430009fbaa7198
Slobodan Mikarić <smikaric@msn.com>
2020-01-26 20:46:01 +0100

    Added modeless window and dynamic selection

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using GetSlabReinfResult;
+using System;
+using RobotOM;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            GetSlabReinfResult.Services.RobotAppService.iapp = new RobotApplication();
+            var w = new MainWindow();
+            w.ShowDialog();
+        }
+    }
+}
